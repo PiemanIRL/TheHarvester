922463c52ab2dbd0b48dccb06c7e96b49ca82962
nasermily <nja9798@g.rit.edu>
2022-01-02 20:28:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from discord import Embed
 import datetime
 from pytz import timezone
 
-BOT_TOKEN = os.environ['botToken']
+#BOT_TOKEN = os.environ['botToken']
+BOT_TOKEN = os.getenv('botToken')
 Client = discord.Client()
 Channel_ID = 927097884564922391 #Needs to be changed if channel changes/is deleted
 Tourney_Role_1 = "2nd Chancers"
