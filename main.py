1e512607b71474dc5c82d683cfdd907b34551a68
PiemanIRL <97004463+PiemanIRL@users.noreply.github.com>
2022-01-05 22:19:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ import datetime
 from time import sleep
 from pytz import timezone
 
+#All 'custom plugins imported below
 import TournyManager
+
+
+
 
 BOT_TOKEN = os.environ['botToken']
 Client = discord.Client()
