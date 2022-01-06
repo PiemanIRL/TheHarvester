05de4420729746edcdd3005ae4361ccaeeda3fe9
PiemanIRL <97004463+PiemanIRL@users.noreply.github.com>
2022-01-05 22:52:42 -0500

    Update MusicBot.py

--- MusicBot.py
+++ MusicBot.py
@@ -54,7 +54,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
       
 #-----------------------------------------------------------------------------------------------------------
       
-      @bot.command(name='join', help='Tells the bot to join the voice channel')
+@bot.command(name='join', help='Tells the bot to join the voice channel')
 async def join(ctx):
     if not ctx.message.author.voice:
         await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
